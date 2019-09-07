939957c1aa3c040b2a3086c2c92a82f7d81cdece
luohong <luohong@neusoft.com>
2019-09-07 23:17:34 +0800

    add echarts

--- leetcode/tree/107.二叉树的层次遍历-ii.js
+++ leetcode/tree/107.二叉树的层次遍历-ii.js
@@ -42,11 +42,34 @@
  *     this.left = this.right = null;
  * }
  */
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrderBottom = function(root) {
-    
+var levelOrderBottom = function (root) {
+    // 队列
+    var queue = [];
+    var result = [];
+    if (root !== null) {
+        queue.push(root);
+    }
+    while (queue.length !== 0) {
+        var level = [];
+        var len = queue.length;
+        for (var i = 0; i < len; i++) {
+            var currentNode = queue.shift();
+            level.push(currentNode.val);
+            if (currentNode.left !== null) queue.push(currentNode.left);
+            if (currentNode.right !== null) queue.push(currentNode.right);
+        }
+        result.push(level);
+    }
+    return result.reverse();
 };
-
+levelOrderBottom([3, 9, 20, null, null, 15, 7])
