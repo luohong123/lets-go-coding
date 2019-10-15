bcf6baa88a317a704bc653d7edce0d5ca32e42ca
luohong123 <3300536651@qq.com>
2019-10-15 17:09:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:luohong123/https://github.com/luohong123/lets-go-coding.git master:gh-pages
+git push git@github.com:luohong123/https://github.com/luohong123/lets-go-coding.git master:gh-pages
 
 sudo cd -
---
+++ docs/command/mac.md
@@ -0,0 +1,15 @@
+## mac 下 yunx .sh 脚本
+
+```
+在终端进入脚本所在路径，
+
+在终端输入：
+
+./deploy.sh
+
+如果出现 permission denied，就重新设置一下权限再运行，
+
+chmod 777 deploy.sh
+
+./deploy.sh
+```
